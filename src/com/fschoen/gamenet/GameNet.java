26301e7235496ac216b7b6a53c3075bc58bfbd9e
BuildTools <unconfigured@null.spigotmc.org>
2019-09-15 19:51:55 +0200

    Initial commit

---
+++ src/com/fschoen/gamenet/GameNet.java
@@ -0,0 +1,17 @@
+package com.fschoen.gamenet;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GameNet extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // On server start
+    }
+
+    @Override
+    public void onDisable() {
+        // On server stop
+    }
+
+}
