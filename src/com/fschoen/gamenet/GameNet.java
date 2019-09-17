0c760d8e34afd7e4fffbef35a284d2e89fb0df71
BuildTools <unconfigured@null.spigotmc.org>
2019-09-17 14:02:17 +0200

    Refactor TeleportCommand to WarpCommand Change config generation

--- src/com/fschoen/gamenet/GameNet.java
+++ src/com/fschoen/gamenet/GameNet.java
@@ -1,20 +1,46 @@
 package com.fschoen.gamenet;
 
+import com.fschoen.gamenet.commands.WarpCommand;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GameNet extends JavaPlugin {
 
-    FileConfiguration config = getConfig();
-
     @Override
     public void onEnable() {
-        // On server start
+        FileConfiguration config = this.getConfig();
+        this.setConfigDefaults(config);
+        this.registerCommands();
+        this.registerListeners();
     }
 
     @Override
     public void onDisable() {
         // On server stop
+    }
+
+    /**
+     * Copies the given defaults into the configuration file and saves the file. Does not overwrite changed attributes.
+     * @param config Configuration file of the plugin
+     */
+    private void setConfigDefaults(FileConfiguration config) {
+        this.saveDefaultConfig();
+        config.options().copyDefaults(true);
+        saveConfig();
+    }
+
+    /**
+     * Registers all commands.
+     */
+    private void registerCommands() {
+        this.getCommand("gwarp").setExecutor(new WarpCommand());
+    }
+
+    /**
+     * Registers all listeners.
+     */
+    private void registerListeners() {
+        //TODO
     }
 
     public static String NO_PERMISSION = "You are not permitted to execute this command.";
--- src/com/fschoen/gamenet/commands/WarpCommand.java
+++ src/com/fschoen/gamenet/commands/WarpCommand.java
@@ -6,13 +6,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class TeleportCommand implements CommandExecutor {
-
-    private GameNet main;
-
-    public TeleportCommand(GameNet main) {
-        this.main = main;
-    }
+public class WarpCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
